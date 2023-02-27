9c25e731c7787633b2e6d89e7e5ceb75ef74176f
VincenzoH <91215243+HOPierre-Louis@users.noreply.github.com>
2023-02-27 13:14:15 +0100

    Update anime.service.js

--- frontend/src/services/anime.service.js
+++ frontend/src/services/anime.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = "http://localhost:5000";
+const backendUrl = "http://localhost:5001/predict";
 
 export const sendRequest = async (data) =>{
     const response = await axios.post(backendUrl, data)
